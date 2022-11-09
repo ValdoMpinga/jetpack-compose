e8ab45a61f880e783b75db9ed258f8fa06d93d32
Valdo Mpinga <valdompinga57@gmail.com>
2022-11-09 17:44:18 +0000

    done

--- app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jatpackcomposecourse.ui.theme.JatpackComposeCourseTheme
 
 class MainActivity : ComponentActivity()
@@ -18,23 +25,24 @@ class MainActivity : ComponentActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContent {   
-            Greeting(name = "James Guevara")
+        setContent {
+            Column(modifier = Modifier
+                .background(Color.Green)
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+                .border(5.dp,Color.Yellow)
+                .padding(5.dp)
+                .border(5.dp,Color.LightGray)
+                .padding(5.dp)
+                .border(8.dp,Color.Red)
+                .padding(115.dp)
+      //          .requiredWidth(300.dp)
+            )
+            {
+                Text("Hello", modifier =Modifier.clickable {  })
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("World")
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String)
-{
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview()
-{
-    JatpackComposeCourseTheme {
-        Greeting("Awesome")
     }
 }
