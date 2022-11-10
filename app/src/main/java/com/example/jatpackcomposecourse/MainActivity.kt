8d11fad47314d7c344b336d9f8eae3bd5917fb33
Valdo Mpinga <valdompinga57@gmail.com>
2022-11-10 14:44:18 +0000

    done

--- app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
@@ -1,40 +1,68 @@
 package com.example.jatpackcomposecourse
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jatpackcomposecourse.ui.theme.JatpackComposeCourseTheme
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContent {   
-            Greeting(name = "James Guevara")
+
+
+        setContent {
+
+            Column(Modifier.fillMaxSize())
+            {
+                val color = remember { mutableStateOf(Color.Yellow) }
+
+                ColorBox(
+                    Modifier
+                        .fillMaxSize()
+                        .weight(1f))
+                {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
+
         }
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun Greeting(name: String)
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit)
 {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview()
-{
-    JatpackComposeCourseTheme {
-        Greeting("Awesome")
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(), 1f
+                )
+            )
+
+        }
+    ) {
     }
 }
