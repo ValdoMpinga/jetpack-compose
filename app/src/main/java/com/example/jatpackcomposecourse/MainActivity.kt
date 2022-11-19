5f9962240c7f6f868360b51511feb4bd60df3f23
Valdo Mpinga <valdompinga57@gmail.com>
2022-11-19 15:56:40 +0000

    done

--- app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jatpackcomposecourse.ui.theme.JatpackComposeCourseTheme
 
 class MainActivity : ComponentActivity()
@@ -18,23 +29,51 @@ class MainActivity : ComponentActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContent {   
-            Greeting(name = "James Guevara")
+        setContent {
+            var sizeState by remember { mutableStateOf(200.dp) }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+//                tween(
+//                    durationMillis = 3000,
+//                    delayMillis =300,
+//                    easing = LinearOutSlowInEasing
+//                )
+//            spring(
+//                Spring.DampingRatioMediumBouncy
+//            ),
+                keyframes{
+                    durationMillis = 5000
+                    sizeState at 0 with LinearEasing
+                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                    sizeState * 2f at 5000
+                }
+            )
+
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            )
+            {
+                Button(onClick = {sizeState += 50.dp}) {
+                    Text(text = "Increase size")
+                }
+            }
+
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String)
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green
+)
 {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview()
-{
-    JatpackComposeCourseTheme {
-        Greeting("Awesome")
-    }
 }
