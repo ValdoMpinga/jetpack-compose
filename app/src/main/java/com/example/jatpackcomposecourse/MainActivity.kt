8d7e5934e69bf893e0608de6f72653f948ce7b54
Valdo Mpinga <valdompinga57@gmail.com>
2022-11-21 18:15:16 +0000

    done

--- app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/example/jatpackcomposecourse/MainActivity.kt
@@ -1,40 +1,100 @@
 package com.example.jatpackcomposecourse
 
+import android.graphics.drawable.Animatable
+import android.media.Image
 import android.os.Bundle
-import android.widget.EditText
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jatpackcomposecourse.ui.theme.JatpackComposeCourseTheme
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContent {   
-            Greeting(name = "James Guevara")
+        setContent {
+            Surface(
+                color = Color(0xFF2020),
+                modifier = Modifier.fillMaxSize())
+            {
+                Navigation()
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String)
+fun Navigation()
 {
-    Text(text = "Hello $name!")
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen") {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+
+        composable("main_screen") {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(
+                    text = "MAIN SCREEN",
+                    color = Color.White)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview()
+fun SplashScreen(navController: NavController)
 {
-    JatpackComposeCourseTheme {
-        Greeting("Awesome")
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+        modifier = Modifier.scale(scale.value))
     }
 }
